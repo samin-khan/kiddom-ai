d50e978113073c5af0289705719bb14cbb4c588f
Samin Khan <saminkhan@DNa224b58.SUNet>
2024-11-08 14:52:02 -0800

    Dashboard bug fix

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -19,7 +19,7 @@ const Progress = React.forwardRef<
   >
     <ProgressPrimitive.Indicator
       className="h-full w-full flex-1 bg-zinc-900 transition-all dark:bg-zinc-50"
-      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+      style={{ transform: `translateX(-${100 - (value ?? 0)}%)` }}
     />
   </ProgressPrimitive.Root>
 ))
